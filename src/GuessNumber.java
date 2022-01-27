6ac295851fd9bf91b715d1ec6a530a4ae11f83a9
aleksandrzh52 <96547517+aleksandrzh52@users.noreply.github.com>
2022-01-27 22:56:25 +0300

    Final

---
+++ src/GuessNumber.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+
+        int max = 5;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите минимальное число: ");
+        int numScannerMin = scanner.nextInt();
+        System.out.print("Введите максимальное число: ");
+        int numScannerMax = scanner.nextInt();
+        System.out.print("Выберите любое число от " + numScannerMin + " до " + numScannerMax + ": ");
+        Random random = new Random();
+        int randInt = random.nextInt(numScannerMin,numScannerMax) + 1;
+
+
+        while (true) {
+            int scannerInt = scanner.nextInt();
+            if (randInt == scannerInt) {
+                System.out.println("Совершенно верно! Это и есть загаданное мною число!");
+                break;
+            } else if (randInt < scannerInt) {
+                System.out.println("Загаданное мною число меньше.");
+            } else {
+                System.out.println("Загаданное мною число больше.");
+            }
+            --max;
+            if (max != 0) {
+                System.out.print("Попробуйте еще раз: ");
+            } else {
+                System.out.print("Увы.. Колличество попыток ограниченно.");
+                break;
+            }
+
+        }
+    }
+}
